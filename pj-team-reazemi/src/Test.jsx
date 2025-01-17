f82b938495a233826541959112defffd1903b867
Sanmoondee11 <sanmoon2001fu@gmail.com>
2025-01-17 10:37:13 +0900

    ログイン画面遷移過程を省きました

--- pj-team-reazemi/src/Test.jsx
+++ pj-team-reazemi/src/Test.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import Ichiran from "./pages/ichiran.jsx";
+import Syosai from "./pages/syosai.jsx";
 
 function Test() {
   return (
     <div>
       <ul>
         <li>
-          <Link to="login">ログイン画面</Link>
+          {/* <Link to="login">ログイン画面</Link> */}
+          <Ichiran />
+          {/* <Syosai /> */}
         </li>
       </ul>
     </div>
